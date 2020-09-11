3c1895ca594e4117fcfb33aa4d81ed779b4d70f5
Yordan Donov <danidonov@gmail.com>
2020-09-11 19:16:01 +0300

    cors

--- src/main/java/event_center/ec/config/CorsConfig.java
+++ src/main/java/event_center/ec/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(false); //updated to false
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://eventcenter-bg.herokuapp.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("GET");
         config.addAllowedMethod("PUT");
